30fc6a21b804e28a5346934427c5f5fad11bbd49
hashdila <dilannawa@gmail.com>
2023-03-26 20:29:40 +0530

    seller start

--- src/main/java/com/example/din/seller/sellerregistration.java
+++ src/main/java/com/example/din/seller/sellerregistration.java
@@ -1,5 +1,7 @@
 package com.example.din.seller;
 
+import com.example.din.DBConnction;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,46 +14,46 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-@WebServlet(name = "sellerRegister", value = "/seller-register")
+@WebServlet(name = "/register", value = "/seller-register")
 public class sellerregistration extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        com.example.projectone.seller.Seller seller = new Seller();
+        com.example.din.seller.Seller seller = new Seller();
         seller.setUsername(request.getParameter("username"));
         seller.setPassword(request.getParameter("password"));
         seller.setEmail(request.getParameter("email"));
 
-        // Hash the password using SHA-256
-        String hashedPassword = hashPassword(seller.getPassword());
-        seller.setPassword(hashedPassword);
+//        // Hash the password using SHA-256
+//        String hashedPassword = hashPassword(seller.getPassword());
+//        seller.setPassword(hashedPassword);
 
         try {
-            Connection conn = DBConnection.getConn();
+            Connection conn = DBConnction.getConn();
             String sql = "INSERT INTO sellers (username, password, email) VALUES (?, ?, ?)";
             PreparedStatement stmt = conn.prepareStatement(sql);
             stmt.setString(1, seller.getUsername());
             stmt.setString(2, seller.getPassword());
             stmt.setString(3, seller.getEmail());
             stmt.executeUpdate();
-            response.sendRedirect("seller-center/success.jsp");
+            response.sendRedirect("./seller/success.jsp");
         } catch (SQLException ex) {
             ex.printStackTrace();
-            response.sendRedirect("error.jsp");
+            response.sendRedirect("./seller/error.jsp");
         }
     }
 
-    private String hashPassword(String password) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            byte[] hashedPasswordBytes = md.digest(password.getBytes());
-            StringBuilder sb = new StringBuilder();
-            for (byte b : hashedPasswordBytes) {
-                sb.append(String.format("%02x", b));
-            }
-            return sb.toString();
-        } catch (NoSuchAlgorithmException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-    }
+//    private String hashPassword(String password) {
+//        try {
+//            MessageDigest md = MessageDigest.getInstance("SHA-256");
+//            byte[] hashedPasswordBytes = md.digest(password.getBytes());
+//            StringBuilder sb = new StringBuilder();
+//            for (byte b : hashedPasswordBytes) {
+//                sb.append(String.format("%02x", b));
+//            }
+//            return sb.toString();
+//        } catch (NoSuchAlgorithmException ex) {
+//            ex.printStackTrace();
+//            return null;
+//        }
+//    }
 }
---
+++ src/main/webapp/seller/success.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Dilan Nawarathna
+  Date: 3/26/2023
+  Time: 3:10 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Registration Successful</title>
+</head>
+<body>
+<h1>Registration Successful</h1>
+<p>Thank you for registering.</p>
+</body>
+</html>
