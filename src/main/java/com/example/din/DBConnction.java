a077b22ed5036243370ff43e90f2cacdd8baa450
Dilan Nawarathna <dilannawa@gmail.com>
2023-04-03 12:10:47 +0530

    seller view

--- src/main/java/com/example/din/DBConnction.java
+++ src/main/java/com/example/din/DBConnction.java
@@ -11,7 +11,7 @@ public class DBConnction {
     public static Connection getConn() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bumblebee", "root", "");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bumble_bee", "root", "");
         } catch (Exception e) {
             e.printStackTrace();
         }
